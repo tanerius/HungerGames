7edd63cb5089f7ebcc92d45fc80666a72224e4d2
Taner Selim <tanerius@gmail.com>
2017-01-23 01:14:06 +0100

    Initial commit

---
+++ Source/HungerGames/HungerGames.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __HUNGERGAMES_H__
+#define __HUNGERGAMES_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
---
+++ Source/HungerGames/HungerGamesGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "HungerGames.h"
+#include "HungerGamesGameMode.h"
+#include "HungerGamesHUD.h"
+#include "HungerGamesCharacter.h"
+
+AHungerGamesGameMode::AHungerGamesGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AHungerGamesHUD::StaticClass();
+}
