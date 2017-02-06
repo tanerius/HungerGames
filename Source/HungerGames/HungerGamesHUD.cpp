d0514ae85395d47cb15d101604cea968adc1b799
Taner Selim <tanerius@gmail.com>
2017-02-06 23:21:52 +0100

    Cleaning up - Part 2

--- Source/HungerGames/HungerGamesHUD.cpp
+++ Source/HungerGames/HungerGamesHUD.cpp
@@ -9,7 +9,7 @@
 AHungerGamesHUD::AHungerGamesHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshiarTexObj.Object;
 }
 
